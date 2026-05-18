4eb2622c3d61e0e1e98fc57f5efa0e8d56e535dc
Ken Gao <ken.gao0216@gmail.com>
2026-05-18 13:41:53 -0400

    Time: 0 ms (100%), Space: 7.9 MB (68.21%) - LeetHub

--- 0202-happy-number/0202-happy-number.cpp
+++ 0202-happy-number/0202-happy-number.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     bool isHappy(int n) {
-        unordered_map<int,int>m;
-        int x = n;
-        while(true){
-            x = f(x);
-            if(x==1) return true;
-            if(m[x]) break;
-            m[x] = 1;
+        int s = n, f = n;
+        s = fun(s), f = fun(fun(f));
+        while(s!=f){
+            s = fun(s);
+            f = fun(fun(f));
         }
-        return false;
+
+        return s == 1;
     }
 
-    int f(int n){
+    int fun(int n){
         int ans = 0;
         while(n>0){
             ans+= (n%10) * (n%10);
