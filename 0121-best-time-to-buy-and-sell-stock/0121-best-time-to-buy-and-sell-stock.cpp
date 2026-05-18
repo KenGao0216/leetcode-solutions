c94c0b3df9a54d66015e50dd7204af68b7176e14
Ken Gao <ken.gao0216@gmail.com>
2026-05-18 14:28:44 -0400

    Time: 0 ms (100%), Space: 97.4 MB (59.8%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -4,9 +4,9 @@ public:
         int n = prices.size();
         int ans = 0;
         int minp = INT_MAX;
-        for(int i = 0; i<n; ++i){
-            minp = min(minp, prices[i]);
-            ans = max(ans, prices[i] - minp);
+        for(int &i:prices){
+            minp = min(minp, i);
+            ans = max(ans, i - minp);
         }
         return ans;
     }
