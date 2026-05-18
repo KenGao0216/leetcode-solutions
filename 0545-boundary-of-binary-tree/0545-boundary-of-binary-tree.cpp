971cb4631cbe1831e0cec9fa7c4e2eedafc48ce7
Ken Gao <ken.gao0216@gmail.com>
2026-05-18 18:31:29 -0400

    Time: 11 ms (3.36%), Space: 21.7 MB (30.58%) - LeetHub

---
+++ 0545-boundary-of-binary-tree/0545-boundary-of-binary-tree.cpp
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int>ans;
+    vector<int>r;
+    vector<int> boundaryOfBinaryTree(TreeNode* root) {
+        if(!root) return ans;
+        ans.push_back(root->val);
+        if(root->left) findLeft(root->left);
+        for(int &i:ans) cout<<i<<" ";
+        cout<<endl;
+        if(root->left || root->right) findLeaves(root);
+        for(int &i:ans) cout<<i<<" ";
+        cout<<endl;
+        if(root->right) findRight(root->right);
+        for(int i = r.size()-1; i>=0; --i) ans.push_back(r[i]);
+        return ans;
+    }
+    void findLeft(TreeNode* root){
+        if(root->left || root->right) ans.push_back(root->val);
+        if(root->left ) findLeft(root->left);
+        else{
+            if(root->right) findLeft(root->right);
+        }
+    }
+    void findLeaves(TreeNode* root){
+        if(!root) return; 
+        if(!root->left && !root->right) {ans.push_back(root->val); return;}
+        findLeaves(root->left);
+        findLeaves(root->right);
+    }
+    void findRight(TreeNode* root){
+        if(root->left || root->right) r.push_back(root->val);
+        if(root->right) findRight(root->right);
+        else{
+            if(root->left) findRight(root->left);
+        }
+    }
+};
