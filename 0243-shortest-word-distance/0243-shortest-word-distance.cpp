f55d18da4dba90c42762273f6effd6d568a3cbf6
Ken Gao <ken.gao0216@gmail.com>
2026-05-18 14:08:06 -0400

    Time: 0 ms (100%), Space: 15.4 MB (97.96%) - LeetHub

---
+++ 0243-shortest-word-distance/0243-shortest-word-distance.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int shortestDistance(vector<string>& wordsDict, string word1, string word2) {
+        int i1 = -1, i2 = -1;
+        int ans = INT_MAX;
+        for(int i = 0; i<wordsDict.size(); ++i){
+            if(word1 == wordsDict[i]) i1 = i;
+            else if(word2 == wordsDict[i]) i2 = i;
+            if(i1!=-1 && i2!=-1) ans = min(ans, abs(i1-i2));
+        }
+        return ans;
+    }
+};
