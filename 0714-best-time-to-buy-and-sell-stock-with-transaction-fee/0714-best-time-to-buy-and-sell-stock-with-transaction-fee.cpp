3d7871c4a65660443b2cffd1c5c1f95e26bc4112
Ken Gao <ken.gao0216@gmail.com>
2026-07-30 11:00:43 -0400

    Time: 2 ms (85.02%), Space: 63.3 MB (99.09%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -6,14 +6,14 @@ public:
         //vector<vector<int>>dp(n, vector<int>(2, -1e6));
         int b = -prices[0];
         int s = 0;
-        int ans = 0;
+        //int ans = 0;
         for(int i = 1; i<n; ++i){
             s = max(s, b+prices[i]-fee);
             b = max(b, s-prices[i]);
-            ans = max(ans, s);
+            //ans = max(ans, s);
             //dp[i][0] = max(dp[i-1][0], dp[i-1][1]+prices[i] - fee);
             //dp[i][1] = max(dp[i-1][1], dp[i-1][0]-prices[i]);
         }
-        return ans;
+        return s;
     }
 };
