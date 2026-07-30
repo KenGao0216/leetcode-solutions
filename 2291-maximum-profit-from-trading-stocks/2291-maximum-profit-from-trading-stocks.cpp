3ac72f7f8421e268016b876728365deb4fb3c05c
Ken Gao <ken.gao0216@gmail.com>
2026-07-30 11:16:48 -0400

    Time: 45 ms (53.17%), Space: 61.1 MB (41.77%) - LeetHub

---
+++ 2291-maximum-profit-from-trading-stocks/2291-maximum-profit-from-trading-stocks.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maximumProfit(vector<int>& present, vector<int>& future, int budget) {
+        int n = present.size();
+        
+        vector<vector<int>>dp(n, vector<int>(budget+1, -1e6));
+        dp[0][0] = 0;
+        if(budget >=present[0] && future[0]-present[0] >0)dp[0][present[0]]=future[0]-present[0];
+        for(int i = 1; i<n; ++i){
+            dp[i] = dp[i-1];
+            if(future[i]<=present[i]) continue;
+            for(int j = budget; j>=present[i]; --j){
+                dp[i][j] = max(dp[i-1][j], dp[i-1][j-present[i]]+future[i]-present[i]);
+            }
+        }
+        return *max_element(dp[n-1].begin(), dp[n-1].end());
+    }
+};
