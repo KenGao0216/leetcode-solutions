3e62085ff434598e6c6a4ba824dd7a7f8be9526e
Ken Gao <ken.gao0216@gmail.com>
2026-08-12 13:28:08 -0400

    Time: 0 ms (100%), Space: 22.3 MB (81.77%) - LeetHub

--- 0845-longest-mountain-in-array/0845-longest-mountain-in-array.cpp
+++ 0845-longest-mountain-in-array/0845-longest-mountain-in-array.cpp
@@ -3,39 +3,24 @@ public:
     int longestMountain(vector<int>& arr) {
         int n = arr.size();
         if(n==1) return 0;
-        vector<int>lis(n, 0);
-        vector<int>lds(n, 0);
-        int x = arr[0];
-        int len = 1;
-        for(int i = 1; i<n; ++i){
-            if(arr[i] > x){
-                x = arr[i];
-                len++;
-                lis[i] = len;
-            }
-            else{
-                len = 1;
-                x = arr[i];
-            }
-        }
-        x = arr[n-1];
-        len = 1;
-        for(int i = n-2; i>=0; --i){
-            if(arr[i] > x){
-                x = arr[i];
-                len++;
-                lds[i] = len;
-            }
-            else{
-                len = 1;
-                x = arr[i];
-            }
-        }
         int ans = 0;
-        for(int i = 0; i<n; ++i) {
-            if(lis[i] > 0 && lds[i] > 0) ans = max(ans, lis[i]+lds[i]-1);
+        int up = 1;
+        int down = 1;
+        int i = 1;
+        while(i<n){
+            while(i<n && arr[i] > arr[i-1]) {up++; i++;}
+            if(up>1){
+                while( i< n && arr[i] < arr[i-1]) {down++; i++;}
+                if(down>1) {
+                    ans = max(ans, up+down-1);
+                    --i;
+                }
+            }
+            down=1;
+            up = 1;
+            ++i;
         }
-        return ans;
+        return ans == 1? 0 :ans;
     }
 };
 
