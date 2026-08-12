a8b2bd837040e4d06cf8d7c479b997e2095a2172
Ken Gao <ken.gao0216@gmail.com>
2026-08-12 13:41:38 -0400

    Time: 0 ms (100%), Space: 22.3 MB (81.77%) - LeetHub

--- 0845-longest-mountain-in-array/0845-longest-mountain-in-array.cpp
+++ 0845-longest-mountain-in-array/0845-longest-mountain-in-array.cpp
@@ -8,19 +8,14 @@ public:
         int down = 1;
         int i = 1;
         while(i<n){
-            while(i<n && arr[i] > arr[i-1]) {up++; i++;}
-            if(up>1){
-                while( i< n && arr[i] < arr[i-1]) {down++; i++;}
-                if(down>1) {
-                    ans = max(ans, up+down-1);
-                    --i;
-                }
-            }
-            down=1;
-            up = 1;
-            ++i;
+            if(arr[i] <= arr[i-1]) {i++; continue;}
+            int start = i-1;
+            while(i<n && arr[i] > arr[i-1]) ++i;
+            if(i==n || arr[i] == arr[i-1]) continue;
+            while( i< n && arr[i] < arr[i-1]) i++;
+            ans = max(ans, i-start);
         }
-        return ans == 1? 0 :ans;
+          return ans;
     }
 };
 
