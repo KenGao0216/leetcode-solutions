81af2e47e6d066864c78b908a83b4e3c3e01eef2
Ken Gao <ken.gao0216@gmail.com>
2026-08-16 18:14:49 -0400

    Time: 79 ms (51.36%), Space: 95.8 MB (15.31%) - LeetHub

--- 0432-all-oone-data-structure/0432-all-oone-data-structure.cpp
+++ 0432-all-oone-data-structure/0432-all-oone-data-structure.cpp
@@ -1,90 +1,120 @@
 class AllOne {
     struct Node{
-        string key;
         int val;
         Node *next;
         Node *prev;
-        Node(string k): key{k}, val{1}, next{nullptr}, prev{nullptr}{}
+        unordered_set<string>keys;
+        Node(int x): val{x}, next{nullptr}, prev{nullptr}{}
     };
    public:
-    unordered_map<string, Node*>m;
+    unordered_map<int, Node*>m;
+    unordered_map<string,int>freq;
     Node *front;
     Node *back;
     AllOne() {
-        front = new Node("1front");
-        back = new Node("1back");
+        front = new Node(1e5);
+        back = new Node(0);
         front->next = back;
         back->prev = front;
     }
     
     void inc(string key) {
-        if(!m[key]){
-            Node *newNode = new Node(key);
-            newNode->prev = back->prev;
-            back->prev->next = newNode;
+        if(!freq[key]){
+            freq[key] = 1;
+            if(!m[1]){
+                Node *newNode = new Node(1);
+                newNode->next = back;
+                newNode->prev = back->prev;
+                back->prev->next = newNode;
+                back->prev = newNode;
 
-            newNode->next = back;
-            back->prev = newNode;
-
-            m[key] = newNode;
+                newNode->keys.insert(key);
+                m[1] = newNode;
+            }
+            else m[1]->keys.insert(key);
         }
         else{
-            Node *cur = m[key];
-            cur->val++;
-            while(cur->prev != front){
-                if(cur->val > cur->prev->val){
-                    Node *cur_prev = cur->prev;
-                    Node *cur_next = cur->next;
+            int oldFreq = freq[key];
+            int newFreq = oldFreq + 1;
 
-                    cur->prev = cur_prev->prev;
-                    cur_prev->prev->next = cur;
+            Node* cur = m[oldFreq];
+            Node* prev = cur->prev; 
 
-                    cur_prev->next = cur_next;
-                    cur_next->prev = cur_prev;
+            cur->keys.erase(key);
+            if (prev != front && prev->val == newFreq) prev->keys.insert(key);
+            
+            else {
+                Node* newNode = new Node(newFreq);
+                newNode->keys.insert(key);
 
-                    cur_prev->prev = cur;
-                    cur->next = cur_prev;
-                }
-                else break;
+                newNode->prev = prev;
+                newNode->next = cur;
+
+                prev->next = newNode;
+                cur->prev = newNode;
+
+                m[newFreq] = newNode;
+            }
+
+            freq[key] = newFreq;
+
+            if (cur->keys.empty()) {
+                cur->prev->next = cur->next;
+                cur->next->prev = cur->prev;
+
+                m.erase(oldFreq);
+                delete cur;
             }
         }
     }
     
     void dec(string key) {
-        Node *cur = m[key];
-        if(cur->val == 1){
-            cur->prev->next = cur->next;
-            cur->next->prev = cur->prev;
-            delete cur;
-            m.erase(key);
-        }
-        else{
-            cur->val--;
-            while(cur->next != back){
-                if(cur->val < cur->next->val){
-                    Node *cur_next = cur->next;
-                    Node *cur_prev = cur->prev;
+    int oldFreq = freq[key];
+    int newFreq = oldFreq - 1;
 
-                    cur->next = cur_next->next;
-                    cur_next->next->prev = cur;
+    Node* cur = m[oldFreq];
+    Node* next = cur->next;   // lower-frequency neighbor
 
-                    cur_prev->next = cur_next;
-                    cur_next->prev = cur_prev;
+    cur->keys.erase(key);
 
-                    cur_next->next = cur;
-                    cur->prev = cur_next;
-                }
-                else break;
-            }
-        }
+    if (newFreq == 0) {
+        freq.erase(key);
     }
+    else {
+        if (next != back && next->val == newFreq) {
+            next->keys.insert(key);
+        }
+        else {
+            Node* newNode = new Node(newFreq);
+            newNode->keys.insert(key);
+
+            newNode->prev = cur;
+            newNode->next = next;
+
+            cur->next = newNode;
+            next->prev = newNode;
+
+            m[newFreq] = newNode;
+        }
+
+        freq[key] = newFreq;
+    }
+
+    if (cur->keys.empty()) {
+        cur->prev->next = cur->next;
+        cur->next->prev = cur->prev;
+
+        m.erase(oldFreq);
+        delete cur;
+    }
+}
     
     string getMaxKey() {
-        return front->next != back ? front->next->key : "";
+        return front->next != back ? *front->next->keys.begin() : "";
     }
     
     string getMinKey() {
-        return back->prev != front ? back->prev->key : "";
+        return back->prev != front ? *back->prev->keys.begin() : "";
     }
 };
 
