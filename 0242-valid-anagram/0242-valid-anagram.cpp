ee4008da8a1a4f801ac197340f49ceba50bce8d7
Ken Gao <ken.gao0216@gmail.com>
2026-08-17 16:04:50 -0400

    Time: 0 ms (100%), Space: 9.8 MB (49.83%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length()!=t.length()) return false;
+        unordered_map<char,int>s1;
+        unordered_map<char,int>t1;
+        for(int i = 0; i<s.length(); ++i) s1[s[i]]++;
+        for(int i = 0; i<t.length(); ++i) t1[t[i]]++;
+        for(auto [x,y]:s1){
+            if(t1[x]!=y) return false;
+        }
+        return true;
+    }
+};
