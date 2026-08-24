ea86bc26116f8b53da608a081a346104a6c4ca8d
Ken Gao <ken.gao0216@gmail.com>
2026-08-24 12:17:23 -0400

    Time: 59 ms (35.38%), Space: 94 MB (38.94%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
@@ -3,22 +3,24 @@ public:
     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
         if(intervals.size() == 1) return 0;
         sort(intervals.begin(), intervals.end(), [](const vector<int>&a, const vector<int>&b) {
-                if(a[0] < b[0]) return a[0] < b[0];
-                else if(a[0] == b[0]) return a[1] < b[1];
-                else return b[0] > a[0];
+                if(a[1] < b[1]) return a[1] < b[1];
+                else if(a[1] == b[1]) return a[0] < b[0];
+                else return b[1] > a[1];
         });
         // for(auto &i: intervals){
         //     cout<<"["<<i[0]<<","<<i[1]<<"], ";
         // }
         int n = intervals.size();
-        vector<int>ends;
-        for(int i = 0; i<n; ++i){
-            int j = upper_bound(ends.begin(), ends.end(), intervals[i][0]) - ends.begin();
-            if(j != ends.size())  ends[j] = min(ends[j], intervals[i][1]);
-            else ends.push_back(intervals[i][1]);
-            
+        int e = intervals[0][1];
+        int ans = 0;
+        for(int i = 1; i<n; ++i){
+            if(intervals[i][0] >=e) e = intervals[i][1];
+            else {
+                ans++;
+                e = min(e, intervals[i][1]);
+            }
         }
-        return n-ends.size();
+        return ans;
 
 
     }
