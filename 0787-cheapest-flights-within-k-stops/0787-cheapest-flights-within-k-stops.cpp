62e7aacf56684b6e78824b797facbb47f16e97ee
Ken Gao <ken.gao0216@gmail.com>
2026-08-24 17:39:33 -0400

    Time: 0 ms (100%), Space: 17.8 MB (73.34%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -1,23 +1,27 @@
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        vector<vector<int>>cost(n, vector<int>(k+2,INT_MAX));
+        vector<int>cost(n, INT_MAX);
         vector<vector<pair<int,int>>>g(n);
         for(auto &f:flights) g[f[0]].push_back({f[1], f[2]});
         int ans = INT_MAX;
-        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>>pq;
-        pq.push({0, src, 0});
-        while(!pq.empty()){
-            auto cur = pq.top();
-            pq.pop();
-            if(cur[2] <=k+1 && cur[1] == dst) ans = min(ans, cur[0]);
-            for(auto nxt: g[cur[1]]){
-                if(cur[2] <=k && cost[nxt.first][cur[2]+1] > cur[0] + nxt.second){
-                    cost[nxt.first][cur[2]+1] = cur[0]+nxt.second;
-                    pq.push({cost[nxt.first][cur[2]+1], nxt.first, cur[2]+1});
+        queue<pair<int,int>>q;
+        q.push({src, 0});
+        int stops = 0;
+        while(!q.empty() && stops <=k){
+            int sz = q.size();
+            while(sz--){
+                auto cur = q.front();
+                q.pop();
+                for(auto nxt: g[cur.first]){
+                    if(cost[nxt.first]> cur.second + nxt.second){
+                        cost[nxt.first] = cur.second + nxt.second;
+                        q.push({nxt.first, cost[nxt.first]});
+                    }
                 }
             }
+            stops++;
         }
-        return ans == INT_MAX? -1 : ans;
+        return cost[dst] == INT_MAX? -1 : cost[dst];
     }
 };
