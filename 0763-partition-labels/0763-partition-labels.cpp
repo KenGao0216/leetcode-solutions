32557063803b9df0457de93f781e805be3da6ea2
Ken Gao <ken.gao0216@gmail.com>
2026-09-10 16:01:25 -0400

    Time: 96 ms (5.34%), Space: 10.8 MB (5.32%) - LeetHub

---
+++ 0763-partition-labels/0763-partition-labels.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<int> partitionLabels(string s) {
+        vector<int>ans;
+        unordered_map<char, vector<int>>m;
+        for(int i = 0; i<s.length(); ++i){
+            m[s[i]].push_back(i);
+        }
+        vector<pair<int,int>>v;
+        for(auto[x,y]:m){
+            cout<<x<<": ";
+            for(int z:y) cout<<z<<" ";
+            cout<<endl;
+            v.push_back({y[0], y.back()});
+        }
+        sort(v.begin(), v.end());
+        for(auto x:v) cout<<"("<<x.first<<","<<x.second<<") ";
+        int l = v[0].first, r = v[0].second;
+        for(auto x:v){
+            if(x.first >= l && x.first <= r && x.second > r){
+                r = x.second;
+            }
+            else if(x.first > r) {
+                ans.push_back(r-l+1);
+                l = x.first;
+                r = x.second;
+            }
+        }
+        ans.push_back(r-l+1);
+        return ans;
+    }
+};
+
+//(0,8) (1,5) (4,7) (9,14) (10,15) (11,11) (13,13) (16,19) (17,22) (18,23) (20,20) (21,21) 
