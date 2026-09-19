c76fabde686cef30ab6d1c23f1fc135b4f993fd7
Ken Gao <ken.gao0216@gmail.com>
2026-09-19 17:52:06 -0400

    Time: 11 ms (98.54%), Space: 60.9 MB (42.65%) - LeetHub

---
+++ 2560-house-robber-iv/2560-house-robber-iv.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int minCapability(vector<int>& nums, int k) {
+        int n = nums.size();
+        int l = *min_element(nums.begin(), nums.end());
+        int r = *max_element(nums.begin(), nums.end());
+        int ans = r;
+        while(l<=r){
+            int m = (l+r)/2;
+            if(f(m, nums) >=k){
+                ans = min(ans, m);
+                r = m-1;
+            }
+            else l = m+1; 
+        }
+        return ans;
+    }
+
+    int f(int m, vector<int>&nums){
+            int cnt = 0;
+            for(int i = 0; i<nums.size(); ++i){
+                if(nums[i] <=m) {cnt++; i++;}
+            }
+            return cnt;
+
+    }
+};
+
+
